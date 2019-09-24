6fc90aa34e34b8e462f0b370f48bb2d54c837e12
bradmendolla <bradmendolla@gmail.com>
2019-09-24 13:43:59 -0400

    added data

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+var new_row = tbody.append("tr")
 // YOUR CODE HERE!
+tableData.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var new_row = tbody.append("tr")
+        Object.entries(ufoReport).forEach(([key, value]) => {
+            var cell = new_row.append("td");
+            cell.text(value);
+        });
+});
